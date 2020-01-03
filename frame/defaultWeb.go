bd1e157f45d6f298d6a9061fd4dff207edd7659d
296270991@qq.com <296270991@qq.com>
2020-01-03 15:19:48 +0800

    fixed

--- frame/defaultWeb.go
+++ frame/defaultWeb.go
@@ -4,8 +4,6 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
-	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 	"github.com/yamakiller/magicLibs/args"
 	"github.com/yamakiller/magicLibs/envs"
 	"github.com/yamakiller/magicLibs/logger"
@@ -36,43 +34,6 @@ func (slf *DefaultWeb) Engine() *gin.Engine {
 //@Summary start system
 //@Return (error) start fail returns error
 func (slf *DefaultWeb) Start() error {
-	release := args.Instance().GetBoolean("-release", false)
-	logPath := args.Instance().GetString("-logPath", "")
-	logSize := args.Instance().GetInt("-logSize", 128)
-
-	slf._log = logger.New(func() logger.Logger {
-		l := logger.LogContext{}
-		l.SetFilPath(logPath)
-		l.SetHandle(logrus.New())
-		l.SetMailMax(logSize)
-		if release {
-			l.SetLevel(logrus.ErrorLevel)
-		} else {
-			l.SetLevel(logrus.InfoLevel)
-		}
-
-		formatter := new(prefixed.TextFormatter)
-		formatter.FullTimestamp = true
-		formatter.TimestampFormat = "2006-01-02 15:04:05"
-		formatter.DisableColors = true
-		formatter.SetColorScheme(&prefixed.ColorScheme{
-			PrefixStyle: "blue+b"})
-
-		l.SetFormatter(formatter)
-		l.Initial()
-		l.Redirect()
-		return &l
-	})
-
-	logger.WithDefault(slf._log)
-	slf._log.Mount()
-
-	if !release {
-		gin.SetMode(gin.DebugMode)
-	} else {
-		gin.SetMode(gin.ReleaseMode)
-	}
-
 	//------------------------------
 	//create http router
 	slf._router = gin.Default()
