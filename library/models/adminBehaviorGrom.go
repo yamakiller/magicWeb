075a349db0d84e842cd43a6815940ea808087167
yamakiller <296270991@qq.com>
2020-02-01 23:18:00 +0800

    修改字符长度

--- library/models/adminBehaviorGrom.go
+++ library/models/adminBehaviorGrom.go
@@ -4,8 +4,8 @@ import "time"
 
 //AdminBehavior admin user behavior
 type AdminBehavior struct {
-	ID         string    `gorm:"primary_key;type:varchar(36);not null;"`
-	OperUserID string    `gorm:"type:varchar(36);index;not null;"`
-	Behavior   string    `gorm:"type:varchar(1024);not null;"`
-	CreatedAt  time.Time `gorm:"index:idx_behavior_time;not null;"`
+	ID        string    `gorm:"primary_key;type:varchar(36);not null;"`
+	Operator  string    `gorm:"type:varchar(36);index;not null;"`
+	Behavior  string    `gorm:"type:varchar(1024);not null;"`
+	CreatedAt time.Time `gorm:"index:idx_behavior_time;not null;"`
 }
--- library/models/adminUserGorm.go
+++ library/models/adminUserGorm.go
@@ -5,14 +5,14 @@ import "time"
 //AdminUser Table
 type AdminUser struct {
 	ID              string    `gorm:"primary_key;type:varchar(36);not null;"`
-	Account         string    `gorm:"type:varchar(32);not null;index:account_idx;"`
+	Account         string    `gorm:"type:varchar(64);not null;index:account_idx;"`
 	Password        string    `gorm:"type:varchar(64);not null;"`
 	Secret          string    `gorm:"type:varchar(16);not null;"`
 	Nick            string    `gorm:"type:varchar(16);not null;"`
 	Email           string    `gorm:"type:varchar(128);not null;"`
-	Mobile          string    `gorm:"type:varchar(20);not null;"`
-	Identity        string    `gorm:"type:varchar(20);not null;"`
-	Roles           string    `gorm:"type:varchar(256);index;not null;"`
+	Mobile          string    `gorm:"type:varchar(32);not null;"`
+	Identity        string    `gorm:"type:varchar(32);not null;"`
+	Roles           string    `gorm:"type:varchar(1024);index;not null;"`
 	Backstage       uint8     `gorm:"not null;"`
 	State           uint8     `gorm:"not null;"`
 	Source          string    `gorm:"type:varchar(64);not null;"`
